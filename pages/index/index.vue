c4a5d9197823153be5494608d87a129adb31787a
Tangyincheng <1069252020@qq.com>
2022-03-03 16:25:39 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<view class="intro">小程序模板</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
